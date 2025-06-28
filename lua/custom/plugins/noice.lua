b5af5cc0fb24c35fa0262887abd7c1c3b19dcebc
badmagick 329 <uzairfarooqi@gmail.com>
2025-06-28 21:30:46 +0100

    add noice

---
+++ lua/custom/plugins/noice.lua
@@ -0,0 +1,38 @@
+return {
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  opts = {
+    -- Disable cmdline replacement
+    cmdline = {
+      enabled = false,
+    },
+    -- Disable the custom messages UI
+    messages = {
+      enabled = false,
+    },
+    -- Disable routing them through the notify view
+    notify = {
+      enabled = false,
+    },
+    -- Only enable the LSP documentation border
+    presets = {
+      bottom_search = false, -- keep built-in search UI
+      command_palette = false, -- disable cmdline popup
+      long_message_to_split = false, -- don’t split long messages
+      inc_rename = false, -- disable in-rename dialog
+      lsp_doc_border = true, -- add a border to hover/signature help
+    },
+    -- (Optional) further tweak the hover window
+    lsp = {
+      documentation = {
+        -- you can override any default, e.g. add a semi-transparent background
+        view = 'hover',
+        opts = {
+          border = 'rounded',
+          winblend = 10,
+        },
+      },
+    },
+  },
+}
