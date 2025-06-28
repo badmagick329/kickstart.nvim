6d529f3cc2f4e87bb49853329e8798f7b7eb2ba9
badmagick 329 <uzairfarooqi@gmail.com>
2025-06-28 21:20:16 +0100

    nvim-ts-autotag update

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -10,11 +10,19 @@ return {
     'windwp/nvim-ts-autotag',
     event = 'InsertEnter', -- or 'BufReadPre'
     dependencies = 'nvim-treesitter',
-    config = function()
-      require('nvim-ts-autotag').setup {
-        enable_close = true, -- auto close tags
-        enable_rename = true, -- auto rename on <Change>
-      }
-    end,
+    opts = {
+      -- Defaults
+      enable_close = true, -- Auto close tags
+      enable_rename = true, -- Auto rename pairs of tags
+      enable_close_on_slash = false, -- Auto close on trailing </
+    },
+    -- Also override individual filetype configs, these take priority.
+    -- Empty by default, useful if one of the "opts" global settings
+    -- doesn't work well in a specific filetype
+    per_filetype = {
+      ['html'] = {
+        enable_close = false,
+      },
+    },
   },
 }
